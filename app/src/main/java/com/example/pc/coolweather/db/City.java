4794bce529b4d31fda41cb9bf7e6ffcf4a1cc36b
Colorful-XL <18817316181@163.com>
2018-10-26 21:45:32 +0800

    first

---
+++ app/src/main/java/com/example/pc/coolweather/db/City.java
@@ -0,0 +1,42 @@
+package com.example.pc.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class City extends LitePalSupport {
+    private int id;
+    private String CityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return CityName;
+    }
+
+    public void setCityName(String cityName) {
+        CityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
